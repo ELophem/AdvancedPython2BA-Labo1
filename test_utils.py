24b7c0924e6947b94c98864898c0602d4ff9a463
ELophem <edouarddelophem@gmail.com>
2020-02-13 10:40:29 +0100

    Add Test

--- test_utils.py
+++ test_utils.py
@@ -4,19 +4,18 @@
 
 import unittest
 import utils
+import math
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-        # À compléter...
-        pass
+        self.assertEqual(utils.fact(0), 1)
     
     def test_roots(self):
-        # À compléter...
-        pass
-    
+        self.assertEqual(utils.roots(1,5,0), 2.5 or -2.5)
+
+
     def test_integrate(self):
-        # À compléter...
-        pass
+        self.assertEqual(utils.integrate("x**3-2", 0, 8),1008)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
